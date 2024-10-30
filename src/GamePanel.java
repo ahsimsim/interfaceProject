062cadaa3672a97e482d6885a6d78962ddf7812f
곡성고 상담실 <cgr31537@gmail.com>
2024-10-30 18:02:17 +0900

    2차 수정
    [1] 인터페이스
    [2] 루프 만들기
    
    -- 10/30/24 --

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,19 +1,40 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
-    final int originalTileSzie = 16; // 16x16
+public class GamePanel extends JPanel implements Runnable{
+    final int originalTileSize = 16; // 16x16
     final int scale = 3;
 
-    final int tileSzie = originalTileSzie * scale; // 48x48
+    final int tileSize = originalTileSize * scale; // 48x48
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
-    final int screenWidth = tileSzie * maxScreenCol; //   768px
-    final int screenHeight = tileSzie * maxScreenRow; //  576px
+    final int screenWidth = tileSize * maxScreenCol; //   768px
+    final int screenHeight = tileSize * maxScreenRow; //  576px
+
+    Thread GameThread;
 
     public GamePanel(){
+
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+
     }
+
+    public void startGameThread(){
+        GameThread = new Thread(this);
+        GameThread.start();
+    }
+
+    @Override
+    public void run(){
+        while(GameThread != null){
+
+//            System.out.println("This gmae is looping");
+
+            // 1 UPDATE: update
+
+        }
+    }
+
 }
