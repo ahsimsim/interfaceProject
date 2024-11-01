ded4245b41a6099fa057ef1c0e74d3f8c778b879
허춘삼 <heoi31537@gmail.com>
2024-11-02 00:53:38 +0900

    3차 수정
    [1] 루프문에 업데이트, 페인팅 구현
    [2] 키입력값 받기
    [3] 입력에 따라 위치 변경
    [4] FPS를 통해 속도 제한
    [5] 더 효율적인 FPS 개선
    
    -- 11/2/24 --

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -11,13 +11,27 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; //   768px
     final int screenHeight = tileSize * maxScreenRow; //  576px
 
+    //FPS
+    int fps = 60;
+
+
+    keyHandler keyH = new keyHandler();
     Thread GameThread;
+
+
+    //set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
 
     public GamePanel(){
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
 
     }
 
@@ -26,15 +40,91 @@ public class GamePanel extends JPanel implements Runnable{
         GameThread.start();
     }
 
+//    @Override
+//    public void run(){
+//
+//        double drawnInterval = 1000000000 / fps; //0.01666666 seconds;
+//        double nextDrawnTime = System.nanoTime() + drawnInterval;
+//
+//        while(GameThread != null){
+//
+//            update();
+//
+//            repaint();
+//
+//
+//
+//            try {
+//                double remainingTime = nextDrawnTime - System.nanoTime();
+//                remainingTime = remainingTime / 1000000;
+//
+//                if(remainingTime < 0){
+//                    remainingTime = 0;
+//                }
+//
+//                Thread.sleep((long) remainingTime);
+//
+//                nextDrawnTime += drawnInterval;
+//
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//        }
+//    }
     @Override
     public void run(){
+
+        double drawInetval = 1000000000.0 / fps;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
         while(GameThread != null){
 
-//            System.out.println("This gmae is looping");
+            currentTime = System.nanoTime();
 
-            // 1 UPDATE: update
+            delta += (currentTime - lastTime) / drawInetval;
+
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
 
         }
+
+    }
+
+    public void update(){
+
+        if(keyH.upPressed){
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed){
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed){
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed){
+            playerX += playerSpeed;
+        }
+
+    }
+
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.dispose();
+
     }
 
 }
